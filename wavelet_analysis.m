bfa24dfce5a2da0a3a89e1fe71f5c5508506eef3
Jodie Pall <jodierae.pall@gmail.com>
2018-06-01 16:41:28 +1000

    New CO2 proxy data
    Proxy-CO2 data from Foster et al. (2017) added.

--- wavelet_analysis.m
+++ wavelet_analysis.m
@@ -1,102 +1,106 @@
-% Title: CWT, XWT and WTC analyses of subduction zone toolbox data
+% Title: CWT, XWT and WTC Analysis of proxy-CO2 data and subduction zone
+% lengh data
 % Author: Jodie Pall
+% Last Edit: 28/05/2017
 
-% This script uses the cross wavelet and wavelet coherence toolbox developed by Aslak Grinsted, John Moore and Svetlana Jevrejeva.
-% Citation: Grinsted, A., J. C. Moore, S. Jevrejeva (2004), Application of the cross wavelet transform and wavelet coherence to geophysical time series, 
-% 			Nonlin. Process. Geophys., 11, 561566.  http://grinsted.github.io/wavelet-coherence/
-
-% Last Edit: 2/03/2017
-
-% Load Data
-load co2proxy_pr2011.dat
-load co2proxy_resampled.txt
-load global_sz_length_carbonate_data.dat
-load global_sz_length_data.dat
-seriesname={'CIC subduction zone lengths' 'Proxy-CO_2 record (Park and Royer 2011)' 'Non-CIC subduction zone lengths' 'Global subduction zone lengths'};
-
-acc = global_sz_length_carbonate_data(:,2);
-sz = global_sz_length_data(:,2);
+% % Load SZ Data
+load global_sz_length_carbonate_data_v3.dat
+load global_sz_length_data_v3.dat
+acc = global_sz_length_carbonate_data_v3(:,2);
+sz = global_sz_length_data_v3(:,2);
 non_acc_raw = sz-acc;
+
+% % Load CO2 data
+% load co2proxy_pr2011.dat
+% load co2proxy_resampled.txt
+% co2raw = co2proxy_pr2011(:,2);
+% co2raw_age = co2proxy_pr2011(:,1);
+% co2rs = co2proxy_resampled(:,2);
+% co2rs_age = co2proxy_resampled(:,1);
+data = csvread('foster_royer_lunt17.csv',2,0);
+data410 = data(1:821,:);
+co2 = data410(1:2:end,:); % CO2 data matrix
+co2_age = co2(:,1); % Vector of ages
+co2_hp = co2(:,2); % Highest probability
+co2_lw95 = co2(:,3); %lower 95% bound
+co2_lw68 = co2(:,4); %lower 68% bound
+co2_up68 = co2(:,5); %upper 68% bound
+co2_up95 = co2(:,6); %upper 95% bound
+
+seriesname={'CISZ lengths' 'Proxy-CO_2 record' 'Non-CISZ lengths' 'Global SZ lengths'};
 age = 0:410;
-co2raw = co2proxy_pr2011(:,2);
-co2raw_age = co2proxy_pr2011(:,1);
-co2rs = co2proxy_resampled(:,2);
-co2rs_age = co2proxy_resampled(:,1);
 
-% Resample CO2 proxy data
-co2rs_resampled = resample(co2rs,co2rs_age,1);
-co2_final = co2rs_resampled(1:411);
+% % Resample CO2 proxy data
+% co2rs_resampled = resample(co2rs,co2rs_age,1);
+% co2_final = co2rs_resampled(1:411);
 
-% Resampled and interpolated CO2 data
-figure
-hold on
-grid on
-set(gca,'xtick',0:50:410);
-set(gca,'ytick',0:250:2850);
-set(gca,'fontsize',16);
-set(gca,'XDir','Reverse');
-plot(co2raw_age,co2raw,'+','Markersize',8.0,'Color',[0.75 0 0.75],'LineWidth',2.0)
-plot(co2rs_age,co2rs,'-','Color',[0 0.75 0.75],'LineWidth',2.0)
-plot(age,co2_final,'o-','MarkerSize',4.0,'Color','k','LineWidth',1.5)
-xlim([0 410]);
-xlabel('Age (Ma)')
-ylabel('Atmospheric CO_2 concentration (ppm)')
-legend('Raw data','Averaged value per time period','Resampled','Location','northeast')
+% % Plot resampled and interpolated CO2 data
+% figure
+% hold on
+% grid on
+% set(gca,'xtick',0:50:410);
+% set(gca,'ytick',0:250:2850);
+% set(gca,'fontsize',16);
+% set(gca,'XDir','Reverse');
+% plot(co2raw_age,co2raw,'+','Markersize',8.0,'Color',[0.75 0 0.75],'LineWidth',2.0)
+% plot(co2rs_age,co2rs,'-','Color',[0 0.75 0.75],'LineWidth',2.0)
+% plot(age,co2_final,'o-','MarkerSize',4.0,'Color','k','LineWidth',1.5)
+% xlim([0 410]);
+% xlabel('Age (Ma)')
+% ylabel('Atmospheric CO_2 concentration (ppm)')
+% legend('Raw data','Averaged value per time period','Resampled','Location','northeast')
 
-
-figure
-subplot(2,1,1);
-hold on
-set(gca,'fontsize',16);
-plot(co2raw_age,co2raw,'-','Color',[0.93 0.69 0.13],'LineWidth',1.5)
-plot(co2rs_age,co2rs,'o-','MarkerSize',4,'Color',[0 0.75 0.75],'LineWidth',1.5)
-xlim([0 410]);
-xlabel('Age (Ma)')
-ylabel('Atmospheric CO_2 (ppm)')
-legend('Raw data','Averaged value per time period','Location','northwest')
-subplot(2,1,2);
-hold on
-set(gca,'fontsize',16);
-plot(co2rs_age,co2rs,'Color',[0 0.75 0.75],'LineWidth',1.5)
-plot(age,co2_final,'o-','MarkerSize',4,'Color','k','LineWidth',1.5)
-xlim([0 410]);
-ylim([0 3000]);
-xlabel('Age (Ma)')
-ylabel('Atmospheric CO_2 (ppm)')
-legend('Averaged value per time period','Resampled','Location','northwest')
-
-
-% plot intersecting arc lengths against non-intersecting arc lengths
-figure;
-hold on 
-grid on
-set(gca,'fontsize',16,'FontWeight','Normal');
-set(gca,'XDir','Reverse');
-plot(age,acc,'Color',[0 0.75 0.75],'LineWidth',2);
-plot(age,non_acc_raw,'Color','k','LineWidth',2);
-plot(age,sz,'Color','m','LineWidth',2);
-xlim([0 410]);
-xlabel('Age (Ma)');
-ylabel('Arc Length (km)');
-legend(seriesname{1},seriesname{3}, seriesname{4},'Location','northeast');
+% figure
+% subplot(2,1,1);
+% hold on
+% set(gca,'fontsize',16);
+% plot(co2raw_age,co2raw,'-','Color',[0.93 0.69 0.13],'LineWidth',1.5)
+% plot(co2rs_age,co2rs,'o-','MarkerSize',4,'Color',[0 0.75 0.75],'LineWidth',1.5)
+% xlim([0 410]);
+% xlabel('Age (Ma)')
+% ylabel('Atmospheric CO_2 (ppm)')
+% legend('Raw data','Averaged value per time period','Location','northwest')
+% subplot(2,1,2);
+% hold on
+% set(gca,'fontsize',16);
+% plot(co2rs_age,co2rs,'Color',[0 0.75 0.75],'LineWidth',1.5)
+% plot(age,co2_final,'o-','MarkerSize',4,'Color','k','LineWidth',1.5)
+% xlim([0 410]);
+% ylim([0 3000]);
+% xlabel('Age (Ma)')
+% ylabel('Atmospheric CO_2 (ppm)')
+% legend('Averaged value per time period','Resampled','Location','northwest')
+% 
+% % Plot intersecting arc lengths against non-intersecting arc lengths
+% figure;
+% hold on 
+% grid on
+% set(gca,'fontsize',16,'FontWeight','Normal');
+% set(gca,'XDir','Reverse');
+% plot(age,acc,'Color',[0 0.75 0.75],'LineWidth',2);
+% plot(age,non_acc_raw,'Color','k','LineWidth',2);
+% plot(age,sz,'Color','m','LineWidth',2);
+% xlim([0 410]);
+% xlabel('Age (Ma)');
+% ylabel('Arc Length (km)');
+% legend(seriesname{1},seriesname{3}, seriesname{4},'Location','northeast');
 
 % Detrend data
 acc_detrend = detrend(acc);
-co2_detrend = detrend(co2_final);
 sz_detrend = detrend(sz);
 non_acc_detrend=sz_detrend-acc_detrend;
+co2_detrend = detrend(co2_hp);
 
-% Simple causal moving average filter in Matlab using filter()
-windowSize = 7;
-b = (1/windowSize)*ones(1,windowSize);
+% Simple 5 (Myr) moving average filter in Matlab using filter()
+windowSize = 5;
 a = 1;
+b = (1/windowSize)*ones(1,windowSize);
 
 acc_filt = filter(b,a,acc_detrend);
 acc_filt = acc_filt(windowSize:end);
 co2_filt = filter(b,a,co2_detrend);
 co2_filt = co2_filt(windowSize:end);
 age_filt = age(floor(windowSize/2)+1:end-floor(windowSize/2));
-
 sz_filt = filter(b,a,sz_detrend);
 sz_filt = sz_filt(windowSize:end);
 
